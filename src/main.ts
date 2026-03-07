3941447f53d3776d975f8196bfb7a9a6e7eebc45
Anugera Rejeki Abadi <martin12082000@gmail.com>
2026-03-08 03:09:53 +0700

    fix 17

--- src/main.ts
+++ src/main.ts
@@ -13,7 +13,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   
   app.enableCors({
-    origin: ['http://localhost:3000','http://localhost:3001', 'http://localhost:3002', 'https://www.clickusaha.com','https://management-clickusaha.vercel.app'], // array of allowed origins
+    origin: ['http://localhost:3000','http://localhost:3001', 'http://localhost:3002', 'https://www.clickusaha.com','https://management-clickusaha.vercel.app','https://client-mari-belajar.vercel.app'], // array of allowed origins
     credentials: true, // izinkan kirim cookie/token cross-origin
   });
 
